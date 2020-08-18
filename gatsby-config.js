ddfddf3c6a0353468aa72ddd36172bf57508b77b
Mario Linguito <mariolinguito@gmail.com>
2020-08-18 14:21:16 +0200

    First relase of my personal blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Coding Adventures`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Mario Linguito`,
+      summary: `who lives and works in Neaples, Italy building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `mlinguito`,
     },
   },
   plugins: [
