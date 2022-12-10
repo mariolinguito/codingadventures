3805091b97c622796765ac20d1b18c1b667a8429
Mario Linguito <32237365+mariolinguito@users.noreply.github.com>
2022-12-10 10:47:57 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,10 +15,7 @@ const BlogIndex = ({ data, location }) => {
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
       <Bio />
-      /**
-       * Disabled for now.
-        <Newsletter />
-      */
+       // <Newsletter /> # Disabled for now. Heroku disabled the application. LOL. 
       {posts.map(({ node }) => {
         const title = node.frontmatter.title || node.fields.slug
         return (
