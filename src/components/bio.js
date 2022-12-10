252818ac9c93b1f6860443d2f57508f88bc275fe
Mario Linguito <32237365+mariolinguito@users.noreply.github.com>
2022-12-10 10:34:58 +0100

    Update bio.js

--- src/components/bio.js
+++ src/components/bio.js
@@ -28,7 +28,7 @@ const Bio = () => {
             summary
           }
           social {
-            twitter
+            linkedin
           }
         }
       }
@@ -59,8 +59,8 @@ const Bio = () => {
       <p>
         Written by <strong>{author.name}</strong> {author.summary}
         {` `}
-        <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
+        <a href={`https://www.linkedin.com/in/${social.linkedin}`}>
+          You should follow him on LinkedIn
         </a>
       </p>
     </div>
